71aee4a42be9bf003b3ad3a3c5fe2925a585eaf4
Maria Giovanna Donadio <giorgiavans@MBPQueergiavans.home-life.hub>
2021-05-18 09:49:52 +0200

    pulizia codice

--- CentralineProject/MainGET.java
+++ CentralineProject/MainGET.java
@@ -18,7 +18,7 @@ public class MainGET {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.connect();
-            //Getting the response code
+            
             int responsecode = conn.getResponseCode();
             if (responsecode != 200) {
                 throw new RuntimeException("HttpResponseCode: " + responsecode);
@@ -27,35 +27,16 @@ public class MainGET {
                 String inline = "";
                 Scanner scanner = new Scanner(url.openStream());
 
-                //Write all the JSON data into a string using a scanner
                 while (scanner.hasNext()) {
                     inline += scanner.nextLine();
                 }
 
-                //Close the scanner
                 scanner.close();
 
-                //Using the JSON simple library parse the string into a json object
                 JSONParser parse = new JSONParser();
                 JSONObject data_obj = (JSONObject) parse.parse(inline);
 
-                // //Get the required object from the above created object
-                // JSONObject obj = (JSONObject) data_obj.get("Global");
-
-                // //Get the required data using its key
                 System.out.println(data_obj);
-
-                // JSONArray arr = (JSONArray) data_obj.get("Countries");
-
-                // for (int i = 0; i < arr.size(); i++) {
-
-                //     JSONObject new_obj = (JSONObject) arr.get(i);
-
-                //     if (new_obj.get("Slug").equals("albania")) {
-                //         System.out.println("Total Recovered: " + new_obj.get("TotalRecovered"));
-                //         break;
-                //     }
-                // }
             }
 
         } catch (Exception e) {
