990943c4323ac733394751496ce5583ff6ebf390
Maria Giovanna Donadio <giorgiavans@MBP-Queen-Giorgiavans.local>
2021-05-19 10:07:55 +0200

    pulizia codice

--- MyTimerTask.java
+++ MyTimerTask.java
@@ -18,49 +18,41 @@ public class MyTimerTask extends TimerTask {
     }
 
     private void completeTask() {
+        URL url;
         try {
+   
+            Date date = new Date();      
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date); 
+            int dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
+            url = new URL("https://swh.fi.ibimet.cnr.it:8442/swhrest2/rest/download/j_get_mobile_data/SMART56/2021/"+dayOfYear+"/1/IT");
+   
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
             
-            URL url;
-            try {
-    
-                Date date = new Date();      
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(date); 
-                int dayOfYear = cal.get(Calendar.DAY_OF_YEAR);
-                url = new URL("https://swh.fi.ibimet.cnr.it:8442/swhrest2/rest/download/j_get_mobile_data/SMART56/2021/"+dayOfYear+"/1/IT");
-    
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.connect();
-                
-                int responsecode = conn.getResponseCode();
-                if (responsecode != 200) {
-                    throw new RuntimeException("HttpResponseCode: " + responsecode);
-                    } else {
-    
-                    String inline = "";
-                    Scanner scanner = new Scanner(url.openStream());
-    
-                    while (scanner.hasNext()) {
-                        inline += scanner.nextLine();
-                    }
-    
-                    scanner.close();
-    
-                    JSONParser parse = new JSONParser();
-                    JSONObject data_obj = (JSONObject) parse.parse(inline);
-    
-                    System.out.println(data_obj);
+            int responsecode = conn.getResponseCode();
+            if (responsecode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responsecode);
+                } else {
+   
+                String inline = "";
+                Scanner scanner = new Scanner(url.openStream());
+   
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
                 }
-    
-            } catch (Exception e) {
-                e.printStackTrace();
+   
+                scanner.close();
+   
+                JSONParser parse = new JSONParser();
+                JSONObject data_obj = (JSONObject) parse.parse(inline);
+   
+                System.out.println(data_obj);
             }
-    
-        //assuming it takes 20 secs to complete the task
-            Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+   
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
     
@@ -71,18 +63,13 @@ public class MyTimerTask extends TimerTask {
         timer.scheduleAtFixedRate(timerTask, 0, 10*1000);
         System.out.println("TimerTask started");
         //cancel after sometime
-        try {
+        /*try {
             Thread.sleep(120000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        timer.cancel();
-        System.out.println("TimerTask cancelled");
-        try {
-            Thread.sleep(30000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        timer.cancel(); questo serve per fermare il programma dopo tot millisecondi */
+        
     }
 
 }
